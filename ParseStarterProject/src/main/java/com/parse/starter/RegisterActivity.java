4d32453deaed94ff6cc75f2b0dd8747aecb47247
angshuman <angshuman.halder243@gmail.com>
2017-04-03 17:01:35 +0530

    Added the functionality of registration activity

---
+++ ParseStarterProject/src/main/java/com/parse/starter/RegisterActivity.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+
+package com.parse.starter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseAnalytics;
+
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText password, name, emailId, department, rollNumber;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        name = (EditText) findViewById(R.id.name);
+        emailId = (EditText) findViewById(R.id.emailId);
+        department = (EditText) findViewById(R.id.dept);
+        rollNumber = (EditText) findViewById(R.id.roll_number);
+        password = (EditText) findViewById(R.id.password);
+
+        ParseAnalytics.trackAppOpenedInBackground(getIntent());
+    }
+
+    public void signUp(View view) {
+
+        /*
+        * This function will be called when registration button will be clicked
+        * perform the registration process here
+        * write only the necessary code for registration to the database here
+         */
+
+        EditText confirmPassword = (EditText) findViewById(R.id.confirmPassword);
+
+        // code to check if password and confirm password fields are same or not
+        if( password.getText().toString().equals(confirmPassword.getText().toString())) {
+
+            // write the signup code here
+
+        } else {
+            Toast.makeText(this, "Password field does not match with confirm password", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+        /* code below checks if all the fields are properly working
+
+        Log.i("Info", name.getText().toString());
+        Log.i("Info", emailId.getText().toString());
+        Log.i("Info", department.getText().toString());
+        Log.i("Info", rollNumber.getText().toString());
+        Log.i("Info", password.getText().toString());
+        Log.i("Info", confirmPassword.getText().toString());
+
+        */
+    }
+
+    public void loginActivity(View view) {
+
+        // this function is called when "Member login" Text view is clicked
+
+        Intent memberLoginActivityIntent = new Intent(getApplicationContext(), MemberLoginActivity.class);
+        startActivity(memberLoginActivityIntent);
+    }
+}
