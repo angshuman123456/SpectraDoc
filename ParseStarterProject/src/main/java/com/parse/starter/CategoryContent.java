0f12c9baa04c66d8db44718bc38b0b01279fa032
angshuman <angshuman.halder243@gmail.com>
2017-04-05 05:06:57 +0530

    Milestone - 1. UserInterface design complete.

--- ParseStarterProject/src/main/java/com/parse/starter/CategoryContent.java
+++ ParseStarterProject/src/main/java/com/parse/starter/CategoryContent.java
@@ -3,12 +3,10 @@ package com.parse.starter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class Categories extends AppCompatActivity {
+public class CategoryContent extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_categories);
+        setContentView(R.layout.activity_category_content);
     }
 }
-
