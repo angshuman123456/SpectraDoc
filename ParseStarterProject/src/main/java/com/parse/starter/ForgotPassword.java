2998efe4f82728c96d8819c909de9999adffdfb5
angshuman <angshuman.halder243@gmail.com>
2017-04-05 17:16:46 +0530

    Changed the names and ids of the layouts

--- ParseStarterProject/src/main/java/com/parse/starter/ForgotPassword.java
+++ ParseStarterProject/src/main/java/com/parse/starter/ForgotPassword.java
@@ -5,20 +5,32 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class ForgotPassword extends AppCompatActivity {
 
     Intent memberLoginIntent;
+    EditText emailId, generatedCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
 
+        // gets intent from login activity
         memberLoginIntent = getIntent();
+
+        // inflates the widgets
+        emailId = (EditText) findViewById(R.id.emailId);
+        generatedCode = (EditText) findViewById(R.id.generatedCode);
     }
 
+    // this function will be called when send code button is clicked
     public void sendCode(View view) {
+
+        generatedCode.setVisibility(View.VISIBLE);
+        
+
         Log.i("Info", "Send Code clicked");
     }
 }
