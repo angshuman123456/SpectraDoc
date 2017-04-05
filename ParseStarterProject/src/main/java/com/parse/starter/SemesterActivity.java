2cdf5288ffa16f4798d622d3caff41b448c04e9a
angshuman <angshuman.halder243@gmail.com>
2017-04-05 23:47:40 +0530

    Department Activity functionality is added without the database connectivity

--- ParseStarterProject/src/main/java/com/parse/starter/SemesterActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/SemesterActivity.java
@@ -1,13 +1,22 @@
 package com.parse.starter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class SemesterActivity extends AppCompatActivity {
+
+    Intent getDataFromDepartmentIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_semester);
+
+        getDataFromDepartmentIntent = getIntent();
+        String departmentName = getDataFromDepartmentIntent.getStringExtra("department");
+
     }
+
+
 }
