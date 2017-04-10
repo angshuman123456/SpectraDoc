7f5a7e0feb0e8273980a4a801ecd089ea95ebd16
Richa <ragarwal317@gmail.com>
2017-04-10 19:28:55 +0530

    Added functionality of database in Semester activity of querying the subjects

--- ParseStarterProject/src/main/java/com/parse/starter/SemesterActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/SemesterActivity.java
@@ -3,9 +3,17 @@ package com.parse.starter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class SemesterActivity extends AppCompatActivity {
 
@@ -27,47 +35,70 @@ public class SemesterActivity extends AppCompatActivity {
         String tagId = (String) view.getTag();
 
         if(tagId.equals("1")) {
-            tagId = "semester" + tagId;
+            tagId = "semester-" + tagId;
         }
 
         if(tagId.equals("2")) {
-            tagId = "semester" + tagId;
+            tagId = "semester-" + tagId;
         }
 
         if(tagId.equals("3")) {
-            tagId = "semester" + tagId;
+            tagId = "semester-" + tagId;
         }
 
         if(tagId.equals("4")) {
-            tagId = "semester" + tagId;
+            tagId = "semester-" + tagId;
         }
 
         if(tagId.equals("5")) {
-            tagId = "semester" + tagId;
+            tagId = "semester-" + tagId;
         }
 
         if(tagId.equals("6")) {
-            tagId = "semester" + tagId;
+            tagId = "semester-" + tagId;
         }
 
         if(tagId.equals("7")) {
-            tagId = "semester" + tagId;
+            tagId = "semester-" + tagId;
         }
 
         if(tagId.equals("8")) {
-            tagId = "semester" + tagId;
+            tagId = "semester-" + tagId;
         }
 
         fetchSubjectsName(tagId, departmentName);
     }
 
-    private void fetchSubjectsName(String semester, String department) {
+    private void fetchSubjectsName(final String semester, final String department) {
 
-        ArrayList<String> nameOfSubjects = new ArrayList<>();
+        final ArrayList<String> nameOfSubjects = new ArrayList<>();
 
         /* write the code to connect to db and fetch the subjects of the semester and fill the "nameOfSubjects"
         * with the name of the subjects fetched from the db
          */
+
+        ParseQuery<ParseObject> query1 = ParseQuery.getQuery("subject");
+        query1.whereEqualTo("Dept_Name", department);
+        query1.whereEqualTo("Semester", semester);
+
+        query1.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+
+                Log.i("Info", department);
+                if(e == null && objects.size() > 0) {
+
+
+                    Log.i("Info", "class sort listed according to department name ");
+
+                    for(ParseObject obj: objects) {
+                        Log.i("Info", obj.getString("Subject_Name"));
+                    }
+                }
+            }
+        });
+
+
 
 
         Intent subjectsActivityIntent = new Intent(this, Subject.class);
